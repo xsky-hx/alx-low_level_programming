62187fd4e50169a3361a80458e04c95611b80ede
Sama <sama.hussien77@gmail.com>
2023-04-20 08:59:45 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-/**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
